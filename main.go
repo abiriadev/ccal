512975d677f049b671d17b00ab12237576c698f1
Abiria <abiria.dev@gmail.com>
2023-11-21 11:02:43 +0900

    feat: center calendar title

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"strings"
 	"time"
 )
 
@@ -13,9 +14,30 @@ func weekoffset(year int, month time.Month) time.Weekday {
 	return time.Date(year, month, 0, 0, 0, 0, 0, time.UTC).Weekday()
 }
 
+func center(text string, pad int) string {
+	if len(text) >= pad {
+		return text
+	}
+
+	var buf strings.Builder
+
+	rest := pad - len(text)
+	l, r := rest-rest/2, rest/2
+
+	for _, p := range []string{
+		strings.Repeat(" ", l),
+		text,
+		strings.Repeat(" ", r),
+	} {
+		buf.WriteString(p)
+	}
+
+	return buf.String()
+}
+
 func printMonth(today time.Time) {
 	year, month, _ := today.Date()
-	fmt.Printf("%d %s\n", year, month)
+	fmt.Println(center(fmt.Sprintf("%s %d", month, year), 7*2+6))
 	fmt.Println("Su Mo Tu We Th Fr Sa")
 
 	daysin, weekoffset := daysin(year, month), weekoffset(year, month)
